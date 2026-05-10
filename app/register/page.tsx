3980328ca0e8385c7fd2b9999811181f1e944513
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-05-10 20:25:07 +0300

    Add registration page component

---
+++ app/register/page.tsx
@@ -0,0 +1,18 @@
+export default function Register() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Register</h2>
+
+      <input placeholder="Name" />
+      <br /><br />
+
+      <input placeholder="Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Create Account</button>
+    </div>
+  )
+}
