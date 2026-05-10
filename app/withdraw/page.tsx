d80084e84598f9468709c17dacbb4d895fc75cb5
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-05-10 20:29:09 +0300

    Create Withdraw component for fund withdrawal

---
+++ app/withdraw/page.tsx
@@ -0,0 +1,12 @@
+export default function Withdraw() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Withdraw Funds</h2>
+
+      <input placeholder="Amount" />
+      <br /><br />
+
+      <button>Request Withdraw</button>
+    </div>
+  )
+}
