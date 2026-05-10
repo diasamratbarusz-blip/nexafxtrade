4ce34122bd9480cfe6d42e38e227f70889149dda
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-05-10 20:27:56 +0300

    Create Deposit component for fund deposits

---
+++ app/deposit/page.tsx
@@ -0,0 +1,12 @@
+export default function Deposit() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Deposit Funds</h2>
+
+      <input placeholder="Amount" />
+      <br /><br />
+
+      <button>Pay via M-Pesa</button>
+    </div>
+  )
+}
