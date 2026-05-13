aafa79b0ee1e48a8aa28928b72d450b64c63ad44
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-05-13 19:35:24 +0300

    Enhance login page with improved layout and functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,61 @@
+"use client"
+
+import { useState } from "react"
+import API from "../../lib/api"
+
 export default function Login() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async () => {
+
+    try {
+
+      const res = await API.post("/auth/login", {
+        email,
+        password
+      })
+
+      localStorage.setItem(
+        "token",
+        res.data.token
+      )
+
+      alert("Login Successful")
+
+      window.location.href = "/dashboard"
+
+    } catch (error) {
+      console.log(error)
+      alert("Login Failed")
+    }
+  }
+
   return (
     <div style={{ padding: 40 }}>
-      <h2>Login</h2>
 
-      <input placeholder="Email" />
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
       <br /><br />
 
-      <input type="password" placeholder="Password" />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
       <br /><br />
 
-      <button>Login</button>
+      <button onClick={handleLogin}>
+        Login
+      </button>
+
     </div>
   )
 }
